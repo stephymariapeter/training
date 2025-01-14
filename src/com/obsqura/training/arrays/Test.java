30f0f0c7e30ab03ff5fb73af6b9cec7440fd0367
stephymariapeter <stephymariapeter96@gmail.com>
2025-01-14 21:24:58 +0530

    My initial commit to github

---
+++ src/com/obsqura/training/arrays/Test.java
@@ -0,0 +1,12 @@
+package com.obsqura.training.arrays;
+
+import com.obsqura.training.arrays.Test2;
+public class Test {
+
+	public static void main(String[] args) {
+		Test2 c = new Test2();
+		c.helloTest();
+
+	}
+
+}
