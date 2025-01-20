d88c77e94ef95066c270b9640a52b143bc56bfc7
stephymariapeter <stephymariapeter96@gmail.com>
2025-01-20 18:20:33 +0530

    sampe test 2

---
+++ src/com/obsqura/training/test2/Test2.java
@@ -0,0 +1,11 @@
+package com.obsqura.training.test2;
+
+public class Test2 {
+
+	public static void main(String[]args) {
+		String a = "stephy";
+		int b = 3;
+		System.out.println(" Hai  "+  a + "  Hello  " + b);
+	}
+
+}
